a9065fc5c399a141d576a7190015357b2562fccc
bigpacks <kriderch@gmail.com>
2014-07-20 15:17:19 -0400

    Starting Over

--- project_ear/search/views.py
+++
@@ -1,6 +0,0 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-
-def index(request):
-    context = RequestContext(request)
-    return  render_to_response('search/index.html', context)
